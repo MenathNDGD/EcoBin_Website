53f254e78ca47241a43064b4919d2df828d6129b
Menath Nadungoda <nadungmenath1@gmail.com>
2024-05-12 22:00:29 +0530

    initial commit

---
+++ src/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASGQ_1e3tkNEx_hCClLxinnHoVwTuUMdM",
+  authDomain: "ecobin-3637f.firebaseapp.com",
+  projectId: "ecobin-3637f",
+  storageBucket: "ecobin-3637f.appspot.com",
+  messagingSenderId: "194990816200",
+  appId: "1:194990816200:web:ea74708fd8bb8627af9efd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
