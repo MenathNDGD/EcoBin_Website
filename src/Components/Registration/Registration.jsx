ec9903c93fc16f54fa53b18201821ed3a68dbf0f
Menath Nadungoda <nadungmenath1@gmail.com>
2024-05-14 20:39:53 +0530

    add reg page again

---
+++ src/Components/Registration/Registration.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import './Registration.css'
+import Navbar from '../../Components/Navbar/Navbar'
+import RegistrationImg from '../../assets/RegImg.png'
+import { MdEmail } from "react-icons/md";
+import { FaLock } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaPhoneSquare } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaCity } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+
+const Registration = () => {
+  return (
+    <div className='hero'>
+        <Navbar/>
+        <div className="regContent">
+          <div className="regImg">
+            <img src={RegistrationImg} alt="" />
+          </div>
+          <div className="regForm">
+            <form action=''>
+              <h1>Registration</h1>
+              <div className="inputBox">
+                <input type='text' placeholder='Username' required />
+                <FaUser  className='icon'/>
+              </div>
+              <div className="inputBox">
+                <input type='email' placeholder='Email Address' required />
+                <MdEmail  className='icon'/>
+              </div>
+              <div className="inputBox">
+                <input type='text' placeholder='Contact Number' required />
+                <FaPhoneSquare className='icon'/>
+              </div>
+              <div className="inputBox">
+                <input type='text' placeholder='Address' required />
+                <FaLocationDot  className='icon'/>
+              </div>
+              <div className="inputBox">
+                <input type='text' placeholder='City' required />
+                <FaCity className='icon'/>
+              </div>
+              <div className="inputBox">
+                <input type='password' placeholder='Password' required />
+                <FaLock className='icon'/>
+              </div>
+              <div className="agree">
+                <label><input type='checkbox'/>I Agree to the Terms & Conditions</label>
+              </div>
+              <button type='submit'>Register</button>
+              <div className="loginLink">
+                <p>Already Have an Account? <Link to="/login">Login Now!</Link></p>
+              </div>
+            </form>
+          </div>
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Registration
