2aec3710cd5fa77002b59a6db1846837afff5ea3
Menath Nadungoda <nadungmenath1@gmail.com>
2024-05-17 15:17:14 +0530

    Firebase connected

--- src/Components/Registration/Registration.jsx
+++ src/Components/Registration/Registration.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Registration.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import RegistrationImg from '../../assets/RegImg.png'
@@ -10,8 +10,43 @@ import { FaLocationDot } from "react-icons/fa6";
 import { FaCity } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../../Firebase';
+import { doc, setDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 const Registration = () => {
+  const [Username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async(e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      console.log(user);
+      if (user) {
+        await setDoc(doc(db, "Users", user.uid), {
+          Username: Username,
+          Email: user.email,
+          Contact_Number: contactNumber,
+          Address: address,
+          City: city,
+          Password: password,
+        });
+      }
+      console.log("User Registration is Successfully Completed...!");
+      toast.success("User Registration is Successfully Completed...!", {position: "top-center",});
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message, {position: "top-center",});
+    }
+  };
+
   return (
     <div className='hero'>
         <Navbar/>
@@ -20,30 +55,59 @@ const Registration = () => {
             <img src={RegistrationImg} alt="" />
           </div>
           <div className="regForm">
-            <form action=''>
+            <form onSubmit={handleRegister}>
               <h1>Registration</h1>
               <div className="inputBox">
-                <input type='text' placeholder='Username' required />
+                <input 
+                  type='text'
+                  onChange={(e) => setUsername(e.target.value)} 
+                  placeholder='Username' 
+                  required 
+                />
                 <FaUser  className='icon'/>
               </div>
               <div className="inputBox">
-                <input type='email' placeholder='Email Address' required />
+                <input 
+                  type='email'
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder='Email Address' required 
+                />
                 <MdEmail  className='icon'/>
               </div>
               <div className="inputBox">
-                <input type='text' placeholder='Contact Number' required />
+                <input 
+                  type='text'
+                  onChange={(e) => setContactNumber(e.target.value)} 
+                  placeholder='Contact Number' 
+                  required 
+                />
                 <FaPhoneSquare className='icon'/>
               </div>
               <div className="inputBox">
-                <input type='text' placeholder='Address' required />
+                <input 
+                  type='text'
+                  onChange={(e) => setAddress(e.target.value)} 
+                  placeholder='Address' 
+                  required 
+                />
                 <FaLocationDot  className='icon'/>
               </div>
               <div className="inputBox">
-                <input type='text' placeholder='City' required />
+                <input 
+                  type='text'
+                  onChange={(e) => setCity(e.target.value)} 
+                  placeholder='City' 
+                  required 
+                />
                 <FaCity className='icon'/>
               </div>
               <div className="inputBox">
-                <input type='password' placeholder='Password' required />
+                <input 
+                  type='password'
+                  onChange={(e) => setPassword(e.target.value)} 
+                  placeholder='Password' 
+                  required 
+                />
                 <FaLock className='icon'/>
               </div>
               <div className="agree">
@@ -51,7 +115,7 @@ const Registration = () => {
               </div>
               <button type='submit'>Register</button>
               <div className="loginLink">
-                <p>Already Have an Account? <Link to="/login">Login Now!</Link></p>
+                <p>Already Have an Account? <Link to="/user-login">Login Now!</Link></p>
               </div>
             </form>
           </div>
