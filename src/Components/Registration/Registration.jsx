12a10d624adb7766a92fe52e29ccba327ffae39e
Menath Nadungoda <nadungmenath1@gmail.com>
2024-05-19 15:29:23 +0530

    modified

--- src/Components/Registration/Registration.jsx
+++ src/Components/Registration/Registration.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Registration.css'
 import Navbar from '../../Components/Navbar/Navbar'
-import BackToTop from '../BackToTop/BackToTop.jsx'
+import BackToTop from '../BackToTop/BackToTop'
 import RegistrationImg from '../../assets/RegImg.png'
 import { MdEmail } from "react-icons/md";
 import { FaLock } from "react-icons/fa";
@@ -11,42 +11,8 @@ import { FaLocationDot } from "react-icons/fa6";
 import { FaCity } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import Footer from '../Footer/Footer';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth, db } from '../../Firebase';
-import { doc, setDoc } from 'firebase/firestore';
-import { toast } from 'react-toastify';
 
 const Registration = () => {
-  const [Username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [contactNumber, setContactNumber] = useState("");
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleRegister = async(e) => {
-    e.preventDefault();
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      const user = auth.currentUser;
-      console.log(user);
-      if (user) {
-        await setDoc(doc(db, "Users", user.uid), {
-          Username: Username,
-          Email: user.email,
-          Contact_Number: contactNumber,
-          Address: address,
-          City: city,
-          Password: password,
-        });
-      }
-      console.log("User Registration is Successfully Completed...!");
-      toast.success("User Registration is Successfully Completed...!", {position: "top-center",});
-    } catch (error) {
-      console.log(error.message);
-      toast.error(error.message, {position: "top-center",});
-    }
-  };
 
   return (
     <div className='bgReg'>
@@ -57,12 +23,11 @@ const Registration = () => {
             <img src={RegistrationImg} alt="" />
           </div>
           <div className="regForm">
-            <form onSubmit={handleRegister}>
+            <form>
               <h1>Registration</h1>
               <div className="inputBox">
                 <input 
                   type='text'
-                  onChange={(e) => setUsername(e.target.value)} 
                   placeholder='Username' 
                   required 
                 />
@@ -71,15 +36,14 @@ const Registration = () => {
               <div className="inputBox">
                 <input 
                   type='email'
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder='Email Address' required 
+                  placeholder='Email Address' 
+                  required 
                 />
                 <MdEmail  className='icon'/>
               </div>
               <div className="inputBox">
                 <input 
-                  type='text'
-                  onChange={(e) => setContactNumber(e.target.value)} 
+                  type='text' 
                   placeholder='Contact Number' 
                   required 
                 />
@@ -87,8 +51,7 @@ const Registration = () => {
               </div>
               <div className="inputBox">
                 <input 
-                  type='text'
-                  onChange={(e) => setAddress(e.target.value)} 
+                  type='text' 
                   placeholder='Address' 
                   required 
                 />
@@ -97,7 +60,6 @@ const Registration = () => {
               <div className="inputBox">
                 <input 
                   type='text'
-                  onChange={(e) => setCity(e.target.value)} 
                   placeholder='City' 
                   required 
                 />
@@ -105,8 +67,7 @@ const Registration = () => {
               </div>
               <div className="inputBox">
                 <input 
-                  type='password'
-                  onChange={(e) => setPassword(e.target.value)} 
+                  type='password' 
                   placeholder='Password' 
                   required 
                 />
