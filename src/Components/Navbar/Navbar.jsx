c27af0a8d67958a69c72ad5b20407e48ae329175
Menath Nadungoda <nadungmenath1@gmail.com>
2024-05-26 17:55:27 +0530

    add onClisck function for toggle menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menuIcon from '../../assets/menu_icon.png'
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
@@ -10,17 +11,23 @@ const Navbar = () => {
     window.addEventListener('scroll', ()=>{
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     })
-  },[])
+  },[]);
+
+  const [hideMenu, setHideMenu] = useState(false);
+  const toggleMenu = ()=>{
+    hideMenu ? setHideMenu(false) : setHideMenu(true);
+  }
 
   return (
     <nav className={`container ${sticky? 'darkNav' : ''}`}>
       <img src = {logo} alt = "" className='logo'/>
-      <ul>
+      <ul className={hideMenu ? '' : 'hideMenuIcon'}>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/how-to-use">How to Use</NavLink></li>
         <li><NavLink to="/about-us">About Us</NavLink></li>
         <li><NavLink to="/user-login"><button className='navBtn'>Login</button></NavLink></li>
       </ul>
+      <img src={menuIcon} alt='' className='menuIcon' onClick={toggleMenu}/>
     </nav>
   )
 }
