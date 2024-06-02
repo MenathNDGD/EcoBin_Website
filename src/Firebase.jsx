e91d80def1f67270fccc70228beedf27664eb35e
Menath Nadungoda <nadungmenath1@gmail.com>
2024-06-03 01:02:49 +0530

    modified the firebase configurations

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCHgx5sJ6Y3gMQi3WdvSJZE9Qeb77ZdofU",
   authDomain: "ecobin-login-auth.firebaseapp.com",
@@ -17,10 +12,10 @@ const firebaseConfig = {
   appId: "1:267555158405:web:3a13ab797a9e4bbb45acec"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
 export default app;
